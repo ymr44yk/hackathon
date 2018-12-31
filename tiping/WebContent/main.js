b185d81b93ca9c80f65fe0db2e54f268ec90fefc
ymr44yk <ymr44yk@gmail.com>
2018-12-31 23:45:33 +0900

    対象パネル点灯、Restart、正解数表示

--- tiping/WebContent/main.js
+++ tiping/WebContent/main.js
@@ -6,12 +6,18 @@
 var rand = new Array();
 //の文字列を格納
 var mondai = "";
-//何問目か格納
+//何文字目か格納
 var count = 0;
+//パネルの数
+var queNum = 15;
 //問題数
-var queNum = 9;
-//現在のパネル
-var current = "";
+var qCount = 3;
+//何問目か格納
+var qCurrent = 0;
+//正解数
+var correctCount = 0;
+//不正解回数
+var unCorrectCount = 0;
 
 /*--------------------------------------------*/
 
@@ -44,66 +50,117 @@ var Alphabet = new Array("Ａ","Ｂ","Ｃ","Ｄ","Ｅ","Ｆ","Ｇ","Ｈ","Ｉ",
                          "Ｊ","Ｋ","Ｌ","Ｍ","Ｎ","Ｏ","Ｐ","Ｑ","Ｒ",
                          "Ｓ","Ｔ","Ｕ","Ｖ","Ｗ","Ｘ","Ｙ","Ｚ");
 
+
+//回答ボタン用配列
+//もっといい書き方があるはずだがうまくいかないのでとりあえずこれで。。。
+var Question =　new Array();
+
+var Question1 = {select1:'select1', select2:'select2', select3:'select3', right:'select1'};
+var Question2 = {select1:'select1', select2:'select2', select3:'select3', right:'select2'};
+var Question3 = {select1:'select1', select2:'select2', select3:'select3', right:'select3'};
+
+Question.push(Question1); 
+Question.push(Question2); 
+Question.push(Question3); 
+
 /*--------------------------------------------*/
+//初期化関数
+function initialize(){
+  qCurrent = 0;
+  correctCount = 0;
+  //回答ボタン表示
+  document.getElementById("answerButton1").style.visibility = "visible";
+  document.getElementById("answerButton2").style.visibility = "visible";
+  document.getElementById("answerButton3").style.visibility = "visible";  
+  //Restartボタン表示
+  document.getElementById("restart").style.visibility = "hidden";
+  gameSet();
+}
 
 //タイピングゲームの問題をセットする
 function gameSet(){
+  //終了判定
+  if(qCurrent == qCount){
+    // document.getElementById("container").innerHTML = "終了します。正解数：" + correctCount;
+    //回答ボタン非表示
+    document.getElementById("answerButton1").style.visibility = "hidden";
+    document.getElementById("answerButton2").style.visibility = "hidden";
+    document.getElementById("answerButton3").style.visibility = "hidden";  
+    //Restartボタン表示
+    document.getElementById("restart").style.visibility = "visible";
+    alert("終了します。正解数：" + correctCount); 
+    initialize();
+  }
   //問題文とカウント数をクリアする
   mondai = "";
   count = 0;
+  qCurrent++;
+  unCorrectCount = 0;
   //0～25までの乱数を queNum 個作成して配列randに格納する
   for (var i = 0;i < queNum ;i++){
     rand[i] = Math.floor( Math.random() * 26 );
   }
-    
-  //問題文の作成
-//  for ( var i = 0 ; i < queNum ; i++){
-//    mondai =  mondai + Alphabet[rand[i]];
-//  }
 
+  //パネル作成
   mondai = "<table id='qTable'>";
   
   for(var i = 0; i < 3; i++ ){
     mondai += "<tr>";
-    for(var j = 0; j < 3; j++) {
-      var idNum = i * 3 + j;
+    for(var j = 0; j < 5; j++) {
+      var idNum = i * 5 + j;
       mondai += "<td id='word"+idNum+"' class = 'panel'>"+Alphabet[rand[idNum]]+"</td>";
     }
     mondai += "</tr>";
   }
   mondai += "</table>";
-    
-
   
   //問題枠に表示する
   document.getElementById("container").innerHTML = mondai;
+
+  //回答ボタンに表示する
+  document.getElementById("answerButton1").value = Question[qCurrent-1].select1;
+  document.getElementById("answerButton2").value = Question[qCurrent-1].select2;
+  document.getElementById("answerButton3").value = Question[qCurrent-1].select3;  
+
+  //打鍵対象の色を変える
+  document.getElementById("word0").style.background = "#5bc0de";
 }
 
 //キー入力を受け取る
-function typeGame(){
-    current = "";
-    //入力されたキーコードと、問題文のキーコードを比較
-    if(KEYS[kCode[rand[count]]]){
-        //カウント数を＋１にする
-        count++;
-    
-        //全文字入力したか確認
-        if (count < queNum){
-            //問題文の頭の一文字を切り取る
-            mondai = mondai.substring(1,mondai.Length);
-            //問題枠に表示する
-//            document.getElementById("container").innerHTML = mondai;
-            //打鍵した文字を消す
-            current = "word";
-            current += count-1;
-            document.getElementById(current).style.background = "#ff000000";
-        }else{
-            //問題枠にゲーム終了を表示
-            document.getElementById("container").innerHTML = "終了します";
-        }
+function typeGame() {
+  //入力されたキーコードと、問題文のキーコードを比較
+  if (KEYS[kCode[rand[count]]]) {
+    //カウント数を＋１にする
+    count++;
+    mondai = mondai.substring(1, mondai.Length);
+    //打鍵した文字を消す
+    var current = "word" + (count - 1);
+    var next = "word" + count;
+    document.getElementById(current).style.background = "#ff000000";
+    document.getElementById(next).style.background = "#5bc0de";
+  }
+}
+
+//回答をチェック
+function checkAnswer(){
+  //押されたボタンを取得
+  var selectedAnswer = event.target;
+  //正解
+  if(selectedAnswer.value == Question[qCurrent-1].right) {
+//     alert("Correct!!");
+    correctCount++;
+    gameSet();
+  //不正解
+  } else {
+    alert("Uncorrect!!!!!!!!");
+    unCorrectCount++;
+    //2回不正解したら次の問題にいく
+    if(unCorrectCount == 2){
+      gameSet();
     }
+  }
 }
 
 /*--------------------------------------------*/
-
+//一回目の呼び出し
 gameSet();
